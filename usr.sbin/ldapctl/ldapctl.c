6c0084dafaf04496d00dee0ec48417914ba690e5
Claudio Jeker <claudio@openbsd.org>
2024-08-17 11:04:09 +0200

    Remove unused variable that makes cc whine

--- usr.sbin/ldapctl/ldapctl.c
+++ usr.sbin/ldapctl/ldapctl.c
@@ -145,7 +145,6 @@ index_namespace(struct namespace *ns, const char *datadir)
 	struct cursor		*cursor;
 	struct ber_element	*elm;
 	char			*path;
-	long long int		 ncomplete = 0;
 	int			 i, rc;
 
 	log_info("indexing namespace %s", ns->suffix);
@@ -201,7 +200,6 @@ index_namespace(struct namespace *ns, const char *datadir)
 			btval_reset(&val);
 			if (rc != 0)
 				break;
-			++ncomplete;
 		}
 
 		if (btree_txn_commit(ns->indx_txn) != BT_SUCCESS)
