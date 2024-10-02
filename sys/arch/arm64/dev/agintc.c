3135ddebaaf1e41b658ed72a528cfe2e1f96cd07
Claudio Jeker <claudio@openbsd.org>
2024-10-02 14:37:24 +0200

    Kernel bits for lltrace

--- sys/arch/arm64/dev/agintc.c
+++ sys/arch/arm64/dev/agintc.c
@@ -27,6 +27,7 @@
 #include <sys/malloc.h>
 #include <sys/device.h>
 #include <sys/evcount.h>
+#include <sys/tracepoint.h>
 
 #include <machine/bus.h>
 #include <machine/cpufunc.h>
@@ -1144,7 +1145,11 @@ agintc_run_handler(struct intrhand *ih, void *frame, int s)
 	else
 		arg = frame;
 
+	LLTRACE(lltrace_irq, ih->ih_ipl == IPL_IPI ? LLTRACE_IRQ_IPI : 0,
+	    ih->ih_irq);
 	handled = ih->ih_func(arg);
+	LLTRACE(lltrace_irqret, ih->ih_ipl == IPL_IPI ? LLTRACE_IRQ_IPI : 0,
+	    ih->ih_irq);
 	if (handled)
 		ih->ih_count.ec_count++;
 
@@ -1489,6 +1494,8 @@ agintc_send_ipi(struct cpu_info *ci, int id)
 {
 	struct agintc_softc	*sc = agintc_sc;
 	uint64_t sendmask;
+
+	LLTRACE(lltrace_ipi, ci->ci_cpuid);
 
 	if (ci == curcpu() && id == ARM_IPI_NOP)
 		return;
