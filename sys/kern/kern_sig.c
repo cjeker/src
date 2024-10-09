462fb2c240911027ca0e6fa041501234af21c221
Claudio Jeker <claudio@openbsd.org>
2024-10-09 14:29:12 +0200

    Fix process_stop() to not grab SCHED_LOCK() instead it needs to be called with SCHED_LOCK held. This is because of ptsignal().

--- sys/kern/kern_sig.c
+++ sys/kern/kern_sig.c
@@ -1410,6 +1410,7 @@ cursig(struct proc *p, struct sigctx *sctx, int deep)
 			 * process group, ignore tty stop signals.
 			 */
 			if (prop & SA_STOP) {
+				int wakeparent;
 				mtx_enter(&pr->ps_mtx);
 				if (pr->ps_flags & PS_TRACED ||
 		    		    (pr->ps_pgrp->pg_jobc == 0 &&
@@ -1418,7 +1419,10 @@ cursig(struct proc *p, struct sigctx *sctx, int deep)
 					break;	/* == ignore */
 				}
 				pr->ps_xsig = signum;
-				if (process_stop(p, PS_STOPPED))
+				SCHED_LOCK();
+				wakeparent = process_stop(p, PS_STOPPED);
+				SCHED_UNLOCK();
+				if (wakeparent)
 					process_stopped(p);
 				mtx_leave(&pr->ps_mtx);
 				proc_stop(p, P_SUSPSIG);
@@ -1476,6 +1480,7 @@ int
 proc_trap(struct proc *p, int signum)
 {
 	struct process *pr = p->p_p;
+	int wakeparent;
 
 	mtx_enter(&pr->ps_mtx);
 	while (1) {
@@ -1500,7 +1505,10 @@ proc_trap(struct proc *p, int signum)
 	pr->ps_trapped = p;
 	atomic_setbits_int(&p->p_flag, P_SUSPTRAPPED);
 
-	if (process_stop(p, PS_TRAPPED))
+	SCHED_LOCK();
+	wakeparent = process_stop(p, PS_TRAPPED);
+	SCHED_UNLOCK();
+	if (wakeparent)
 		process_stopped(p);
 	mtx_leave(&pr->ps_mtx);
 
@@ -1545,7 +1553,11 @@ process_stop(struct proc *p, int why)
 	TAILQ_FOREACH(q, &pr->ps_threads, p_thr_link) {
 		if (q == p)
 			continue;
-		SCHED_LOCK();
+		/* XXX right now this is called with the SCHED_LOCK already
+		 * held but in the future that will change.
+		 */
+		/*XXX SCHED_LOCK(); */
+		SCHED_ASSERT_LOCKED();
 		switch (q->p_stat) {
 		case SSTOP:
 			pr->ps_stopcnt--;
@@ -1566,7 +1578,7 @@ process_stop(struct proc *p, int why)
 		case SDEAD:
 			break;
 		}
-		SCHED_UNLOCK();
+		/*XXX SCHED_UNLOCK(); */
 	}
 	/* count ourselfs out */
 	pr->ps_stopcnt--;
