eb6371f4a0369b7a09031bbf6fee915477f961cf
Claudio Jeker <claudio@openbsd.org>
2025-05-08 13:50:30 +0200

    Pass a mutex to mi_switch which is held accross the thread switch and released once the new thread is running. This is needed to use different mutexes to protect the switch.

--- sys/kern/kern_synch.c
+++ sys/kern/kern_synch.c
@@ -402,7 +402,7 @@ sleep_finish(int timo, int do_sleep)
 	if (do_sleep) {
 		KASSERT(p->p_stat == SSLEEP || p->p_stat == SSTOP);
 		p->p_ru.ru_nvcsw++;
-		mi_switch();
+		mi_switch(&sched_lock);
 	} else {
 		KASSERT(p->p_stat == SONPROC || p->p_stat == SSLEEP);
 		p->p_stat = SONPROC;
@@ -671,7 +671,7 @@ sys_sched_yield(struct proc *p, void *v, register_t *retval)
 	SCHED_LOCK();
 	setrunqueue(p->p_cpu, p, newprio);
 	p->p_ru.ru_nvcsw++;
-	mi_switch();
+	mi_switch(&sched_lock);
 	SCHED_UNLOCK();
 
 	return (0);
