b41a9274052754e983ea5bb505a2d2fb986b14ab
Claudio Jeker <claudio@openbsd.org>
2025-05-26 10:38:15 +0200

    Make it mandatory to pass in a struct cpu_info to setrunqueue()

--- sys/kern/kern_sched.c
+++ sys/kern/kern_sched.c
@@ -143,6 +143,7 @@ static void
 sched_purge_queues(struct schedstate_percpu *spc)
 {
 	TAILQ_HEAD(prochead, proc)	pq = TAILQ_HEAD_INITIALIZER(pq);
+	struct cpu_info *ci;
 	struct proc *p;
 	int queue;
 
@@ -156,7 +157,9 @@ sched_purge_queues(struct schedstate_percpu *spc)
 
 	while ((p = TAILQ_FIRST(&pq))) {
 		TAILQ_REMOVE(&pq, p, p_runq);
-		setrunqueue(NULL, p, p->p_runpri);
+
+		ci = sched_choosecpu(p);
+		setrunqueue(ci, p, p->p_runpri);
 		KASSERT(p->p_cpu != curcpu() || p->p_flag & P_CPUPEG);
 	}
 	SCHED_UNLOCK();
@@ -299,9 +302,6 @@ setrunqueue(struct cpu_info *ci, struct proc *p, uint8_t prio)
 {
 	struct schedstate_percpu *spc;
 	int queue = prio >> 2;
-
-	if (ci == NULL)
-		ci = sched_choosecpu(p);
 
 	KASSERT(ci != NULL);
 	SCHED_ASSERT_LOCKED();
--- sys/kern/sched_bsd.c
+++ sys/kern/sched_bsd.c
@@ -452,6 +452,7 @@ void
 setrunnable(struct proc *p)
 {
 	struct process *pr = p->p_p;
+	struct cpu_info *ci;
 	u_char prio;
 
 	SCHED_ASSERT_LOCKED();
@@ -477,7 +478,8 @@ setrunnable(struct proc *p)
 				p->p_stat = SONPROC;
 			return;
 		}
-		setrunqueue(NULL, p, prio);
+		ci = sched_choosecpu(p);
+		setrunqueue(ci, p, prio);
 		break;
 	case SSLEEP:
 		prio = p->p_slppri;
@@ -487,7 +489,8 @@ setrunnable(struct proc *p)
 		/* if not yet asleep, don't add to runqueue */
 		if (ISSET(p->p_flag, P_INSCHED))
 			return;
-		setrunqueue(NULL, p, prio);
+		ci = sched_choosecpu(p);
+		setrunqueue(ci, p, prio);
 		break;
 	}
 	if (p->p_slptime > 1) {
