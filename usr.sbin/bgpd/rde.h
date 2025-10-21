635aaa801a90222498d956b76587a6a6e0ff4136
Claudio Jeker <claudio@openbsd.org>
2025-10-21 20:52:54 +0200

    No need to CH_PROTOTYPE the pend_attr and pend_prefix hash tables in a public header. Just move the initalisation into rde_adjout.c

--- usr.sbin/bgpd/rde.h
+++ usr.sbin/bgpd/rde.h
@@ -776,21 +776,7 @@ int		 adjout_prefix_dump_subtree(struct rde_peer *,
 		    struct bgpd_addr *, uint8_t, unsigned int, void *,
 		    void (*)(struct adjout_prefix *, void *),
 		    void (*)(void *, uint8_t), int (*)(void *));
-
-static inline uint64_t
-pend_prefix_hash(const struct pend_prefix *pp)
-{
-	return ch_qhash64(ch_qhash64(0, pp->path_id_tx), (uintptr_t)pp->pt);
-}
-
-static inline uint64_t
-pend_attr_hash(const struct pend_attr *pa)
-{
-	return ch_qhash64(0, (uintptr_t)pa->attrs);
-}
-
-CH_PROTOTYPE(pend_prefix_hash, pend_prefix, pend_prefix_hash);
-CH_PROTOTYPE(pend_attr_hash, pend_attr, pend_attr_hash);
+void		 adjout_peer_init(struct rde_peer *);
 
 void		 pend_attr_done(struct pend_attr *,
 		    struct pend_attr_queue *, struct rde_peer *);
