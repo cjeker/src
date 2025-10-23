8fee1dad1fd39f1f4418e52d0dddad6faec96e7d
Claudio Jeker <claudio@openbsd.org>
2025-10-23 15:39:32 +0200

    Kill PREFIX_ADJOUT_FLAG_STALE by using a local array to collect all current adjout_prefix entries (actually just their path_id_tx). Also refactor the up_generate_addpath_all() code to just use up_generate_addpath() for the case where all prefixes need to be readded.

--- usr.sbin/bgpd/rde_adjout.c
+++ usr.sbin/bgpd/rde_adjout.c
@@ -565,7 +565,6 @@ adjout_prefix_update(struct adjout_prefix *p, struct rde_peer *peer,
 		    attrs->communities) &&
 		    path_equal(&state->aspath, attrs->aspath)) {
 			/* nothing changed */
-			p->flags &= ~PREFIX_ADJOUT_FLAG_STALE;
 			return;
 		}
 
@@ -573,9 +572,6 @@ adjout_prefix_update(struct adjout_prefix *p, struct rde_peer *peer,
 		adjout_prefix_unlink(p, peer);
 		peer->stats.prefix_out_cnt--;
 	}
-
-	/* clear PREFIX_ADJOUT_FLAG_STALE for up_generate_addpath() */
-	p->flags &= ~PREFIX_ADJOUT_FLAG_STALE;
 
 	/* update path_id_tx now that the prefix is unlinked */
 	if (p->path_id_tx != path_id_tx) {
@@ -616,9 +612,6 @@ adjout_prefix_destroy(struct rde_peer *peer, struct adjout_prefix *p)
 		adjout_prefix_unlink(p, peer);
 		peer->stats.prefix_out_cnt--;
 	}
-
-	/* clear PREFIX_ADJOUT_FLAG_STALE just in case */
-	p->flags &= ~PREFIX_ADJOUT_FLAG_STALE;
 
 	if (!prefix_is_locked(p)) {
 		RB_REMOVE(prefix_index, &peer->adj_rib_out, p);
